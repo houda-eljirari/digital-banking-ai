99b19597aa9b4989eb8f178572f96e60f57eb903
houda-eljirari <eljirarihouda99@gmail.com>
2026-05-09 22:30:36 +0100

    implement customer service layer

---
+++ backend/digital-banking-backend/src/main/java/ma/enset/digitalbankingbackend/services/impl/BankAccountServiceImpl.java
@@ -0,0 +1,74 @@
+package ma.enset.digitalbankingbackend.services.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.enset.digitalbankingbackend.dtos.CustomerDTO;
+import ma.enset.digitalbankingbackend.entities.Customer;
+import ma.enset.digitalbankingbackend.exceptions.CustomerNotFoundException;
+import ma.enset.digitalbankingbackend.mappers.BankAccountMapperImpl;
+import ma.enset.digitalbankingbackend.repositories.CustomerRepository;
+import ma.enset.digitalbankingbackend.services.interfaces.BankAccountService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
+public class BankAccountServiceImpl implements BankAccountService {
+
+    private CustomerRepository customerRepository;
+    private BankAccountMapperImpl mapper;
+
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+
+        log.info("Saving new customer");
+
+        Customer customer = mapper.fromCustomerDTO(customerDTO);
+
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return mapper.fromCustomer(savedCustomer);
+    }
+
+    @Override
+    public List<CustomerDTO> listCustomers() {
+
+        List<Customer> customers = customerRepository.findAll();
+
+        return customers.stream()
+                .map(mapper::fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDTO getCustomer(Long customerId)
+            throws CustomerNotFoundException {
+
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() ->
+                        new CustomerNotFoundException("Customer Not Found"));
+
+        return mapper.fromCustomer(customer);
+    }
+
+    @Override
+    public CustomerDTO updateCustomer(CustomerDTO customerDTO) {
+
+        Customer customer = mapper.fromCustomerDTO(customerDTO);
+
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return mapper.fromCustomer(savedCustomer);
+    }
+
+    @Override
+    public void deleteCustomer(Long customerId) {
+
+        customerRepository.deleteById(customerId);
+    }
+}
