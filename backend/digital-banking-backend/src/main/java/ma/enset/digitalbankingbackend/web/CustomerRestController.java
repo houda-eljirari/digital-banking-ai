f15ab166a55b567e732b0e2e50fc66520360ac31
houda-eljirari <eljirarihouda99@gmail.com>
2026-05-09 22:34:55 +0100

    create customer rest api

---
+++ backend/digital-banking-backend/src/main/java/ma/enset/digitalbankingbackend/web/CustomerRestController.java
@@ -0,0 +1,53 @@
+package ma.enset.digitalbankingbackend.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.digitalbankingbackend.dtos.CustomerDTO;
+import ma.enset.digitalbankingbackend.exceptions.CustomerNotFoundException;
+import ma.enset.digitalbankingbackend.services.interfaces.BankAccountService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/customers")
+    public List<CustomerDTO> customers() {
+
+        return bankAccountService.listCustomers();
+    }
+
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId)
+            throws CustomerNotFoundException {
+
+        return bankAccountService.getCustomer(customerId);
+    }
+
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
+
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+
+    @PutMapping("/customers/{id}")
+    public CustomerDTO updateCustomer(
+            @PathVariable Long id,
+            @RequestBody CustomerDTO customerDTO
+    ) {
+
+        customerDTO.setId(id);
+
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+
+        bankAccountService.deleteCustomer(id);
+    }
+}
