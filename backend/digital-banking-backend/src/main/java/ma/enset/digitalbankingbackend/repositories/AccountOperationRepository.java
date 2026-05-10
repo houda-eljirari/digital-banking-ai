c0ab9162bf47b9446e90f8e5c572f228918e7a0e
houda-eljirari <eljirarihouda99@gmail.com>
2026-05-10 17:15:03 +0100

    Add pagination support for account operations

--- backend/digital-banking-backend/src/main/java/ma/enset/digitalbankingbackend/repositories/AccountOperationRepository.java
+++ backend/digital-banking-backend/src/main/java/ma/enset/digitalbankingbackend/repositories/AccountOperationRepository.java
@@ -1,12 +1,19 @@
 package ma.enset.digitalbankingbackend.repositories;
 
 import ma.enset.digitalbankingbackend.entities.AccountOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AccountOperationRepository
-        extends JpaRepository<AccountOperation,Long> {
+        extends JpaRepository<AccountOperation, Long> {
 
     List<AccountOperation> findByBankAccountId(String accountId);
+
+    Page<AccountOperation> findByBankAccountId(
+            String accountId,
+            Pageable pageable
+    );
 }
